c2fb2c84d325289b079b22ed0a8e72aa583a7bb1
postscriptumno <postscriptum.no@gmail.com>
2021-03-10 11:42:43 +0200

    initial

---
+++ src/components/app/app.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import './app.css';
+import Home from '../home-page/home-page';
+import Results from '../results/results';
+import {AppRoute} from '../../const';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path={AppRoute.RESULTS} exact>
+          <Results />
+        </Route>
+        <Route path={AppRoute.ROOT} exact>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/results/results.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './results.css';
+
+const Results = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Results;
