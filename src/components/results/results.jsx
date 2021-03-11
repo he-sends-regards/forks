6a29b6a1171a18d8371285c10337f6717c0f78c0
postscriptumno <postscriptum.no@gmail.com>
2021-03-11 15:01:34 +0200

    feat: write table of search results

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -1,10 +1,188 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Table, Button} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {useLocation} from 'react-router';
+import PropTypes from 'prop-types';
+import Search from '../search/search';
+import {ActionType} from '../../store/action';
+import {getGithubForks, getGithubRepo} from '../../utils';
 import './results.css';
 
-const Results = () => {
+const Results = ({repo, forks, saveRepo, addForks}) => {
+  const urlParams = new URLSearchParams(useLocation().search);
+
+  const [page, setPage] = useState(
+    urlParams.get('page') &&
+    urlParams.get('owner') &&
+    urlParams.get('repository') ?
+      Number(urlParams.get('page')) :
+      1,
+  );
+
+  const getDataFromParams = async () => {
+    const responseData = await getGithubRepo(
+        urlParams.get('owner'),
+        urlParams.get('repository'),
+    );
+
+    if (responseData) {
+      saveRepo({
+        name: responseData.repository.name,
+        owner: responseData.repository.owner.login,
+        starsCount: responseData.repository.stargazers_count,
+        forksCount: responseData.repository.forks,
+        url: responseData.repository.forks_url,
+      });
+
+      addForks(responseData.forks);
+    }
+  };
+
+  useEffect(() => {
+    if (urlParams.get('owner') && urlParams.get('repository')) {
+      getDataFromParams();
+    }
+  }, []);
+
+  const [areButtonsDisabled, setAreButtonsDisabled] = useState(false);
+
+  // useEffect(() => {
+  //   setPage(1);
+  // }, [repo]);
+
   return (
-    <div></div>
+    <div className="results">
+      <div className="results__header">
+        <Search />
+        <span className="logo">
+        «Forks»
+        </span>
+      </div>
+      {
+        repo ? (
+          <div className="results__body">
+            <Table bordered hover className="results__table-info">
+              <thead>
+                <tr><th colSpan="4">Info</th></tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Repository owner:</th>
+                  <td>{repo.owner}</td>
+                </tr>
+                <tr>
+                  <th>Repository name:</th>
+                  <td>{repo.name}</td>
+                </tr>
+                <tr>
+                  <th>Stars count: </th>
+                  <td>{repo.starsCount}</td>
+                </tr>
+                <tr>
+                  <th>Forks count: </th>
+                  <td>{repo.forksCount}</td>
+                </tr>
+              </tbody>
+            </Table>
+            <Table bordered responsive className="results__table-forks">
+              <thead>
+                <tr><th colSpan="4">Forks (Page #{page})</th></tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Owner</th>
+                  <th>Stars count</th>
+                  <th>Link</th>
+                </tr>
+              </tbody>
+              <tbody>
+                {
+                  forks && forks.length !== 0 && forks.map((fork) => (
+                    <tr key={`${fork.id}`}>
+                      <td>{fork.owner.login}</td>
+                      <td>{fork.stargazers_count}</td>
+                      <td>
+                        <a href={fork.html_url} target="blank">
+                          {fork.html_url}
+                        </a>
+                      </td>
+                    </tr>
+                  ))
+                }
+                <tr>
+                  <th colSpan="4">
+                    {
+                      page > 1 && forks && forks.length !== 0 && (
+                        <Button
+                          variant="primary"
+                          className="flip-btn"
+                          disabled={areButtonsDisabled}
+                          onClick={async () => {
+                            setAreButtonsDisabled(true);
+
+                            const newForks = await getGithubForks(
+                                repo.url,
+                                page - 1,
+                            );
+
+                            addForks(newForks);
+                            setPage(page - 1);
+                            setAreButtonsDisabled(false);
+                          }}
+                        >
+                          &#8592; Back
+                        </Button>
+                      )
+                    }
+                    {
+                      forks && forks.length !== 0 && (<Button
+                        variant="primary"
+                        className="flip-btn"
+                        disabled={areButtonsDisabled}
+                        onClick={async () => {
+                          setAreButtonsDisabled(true);
+
+                          const newForks = await getGithubForks(
+                              repo.url,
+                              page + 1,
+                          );
+
+                          addForks(newForks);
+                          setPage(page + 1);
+
+                          setAreButtonsDisabled(false);
+                        }}
+                      >
+                      Next &#8594;
+                      </Button>)}
+                  </th>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        ) : (
+          'No repo...'
+        )
+      }
+    </div>
   );
 };
 
-export default Results;
+Results.propTypes = {
+  repo: PropTypes.object,
+  forks: PropTypes.array,
+  saveRepo: PropTypes.func.isRequired,
+  addForks: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  repo: state.repo,
+  forks: state.forks,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveRepo: (repo) => dispatch({type: ActionType.SAVE_REPO, payload: repo}),
+  addForks: (forks) => dispatch({type: ActionType.ADD_FORKS, payload: forks}),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Results);
